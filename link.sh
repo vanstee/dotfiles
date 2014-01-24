87d363f9de3f6118cb2da8b3a643081867b42adb
vanstee <patrick@vanstee.me>
2014-01-23 22:49:41 -0500

    Start off our vim config with pathogen

--- link.sh
+++ link.sh
@@ -5,5 +5,8 @@ set -e
 ln -sf $PWD/.gitconfig $HOME
 ln -sf $PWD/.profile $HOME
 ln -sf $PWD/.tmux.conf $HOME
+if [ -d $HOME/.vim ]; then rm -rf $HOME/.vim; fi
+ln -sF $PWD/.vim $HOME/.vim
+ln -sf $PWD/.vimrc $HOME
 
 set +e
